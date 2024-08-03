b5e4a40b09d8cb773bf96aeac0a778262878618d
dan-sheep <dxs138@student.bham.ac.uk>
2024-08-03 18:10:40 +0100

    TicTacToev1

--- secret-hitler-frontend/src/index.js
+++ secret-hitler-frontend/src/index.js
@@ -1,3 +1,5 @@
+// All the preimported crap
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +17,13 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// https://react.dev/learn/tutorial-tic-tac-toe
+// nothing works for below, i think it was just for tutorial crap etc etc
+/*
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './App.css'; //was 'import './styles.css';' but the normal styles in app.css 
+
+import App from './App';
+*/
